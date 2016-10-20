6bf7922cfd9c4b82e8650ec1feacb486908a9a6e
Ed Bowler <ed.bowler@gmail.com>
2016-10-20 16:07:05 +0100

    affect current window

--- add-window-decorations.sh
+++ add-window-decorations.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-xprop -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x1, 0x0, 0x0"
+
+current_focused_window=$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2 )
+
+xprop -id $current_focused_window -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x1, 0x0, 0x0"
--- remove-window-decorations.sh
+++ remove-window-decorations.sh
@@ -2,4 +2,5 @@
 
 ## wmctl -l -x | awk '{print $1}' | xargs -i xprop -id "{}" -f _GTK_HIDE_TITLEBAR_WHEN_MAXIMIZED 32c -set '_GTK_HIDE_TITLEBAR_WHEN_MAXIMIZED' '0x1'
 
-xprop -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x0, 0x0, 0x0"
+current_focused_window=$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2 )
+xprop -id $current_focused_window -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x0, 0x0, 0x0"
