9a7b5cdc57c64451245566a08658015de6bcfe63
Ed Bowler <ed.bowler@gmail.com>
2016-10-20 17:15:36 +0100

    bit more flexible

--- magically-arrange-apps.sh
+++ magically-arrange-apps.sh
@@ -15,26 +15,72 @@ else
     browser=$chrome
 fi
 
-if xprop -id "${emacs}" | grep _NET_WM_STATE_MAXIMIZED_HORZ 2>&1 >/dev/null ; then
-    max_emacs=yes
-else
-    max_emacs=no
-fi
-
 if [ "${emacs}" != "" -a "${terminal}" != "" -a "${browser}" != "" ] ; then
-    if [ "${max_emacs}" == "yes" ] ; then
+    if xprop -id "${emacs}" | grep _NET_WM_STATE_MAXIMIZED_HORZ 2>&1 >/dev/null ; then
+        h_max_emacs=yes
+    else
+        h_max_emacs=no
+    fi
+    if xprop -id "${emacs}" | grep _NET_WM_STATE_MAXIMIZED_VERT 2>&1 >/dev/null ; then
+        v_max_emacs=yes
+    else
+        v_max_emacs=no
+    fi
+    if xprop -id "${browser}" | grep _NET_WM_STATE_MAXIMIZED_HORZ 2>&1 >/dev/null ; then
+        h_max_browser=yes
+    else
+        h_max_browser=no
+    fi
+    if xprop -id "${browser}" | grep _NET_WM_STATE_MAXIMIZED_VERT 2>&1 >/dev/null ; then
+        v_max_browser=yes
+    else
+        v_max_browser=no
+    fi
+    if xprop -id "${terminal}" | grep _NET_WM_STATE_MAXIMIZED_HORZ 2>&1 >/dev/null ; then
+        h_max_terminal=yes
+    else
+        h_max_terminal=no
+    fi
+    if xprop -id "${terminal}" | grep _NET_WM_STATE_MAXIMIZED_VERT 2>&1 >/dev/null ; then
+        v_max_terminal=yes
+    else
+        v_max_terminal=no
+    fi
+    echo "${h_max_emacs}" "${v_max_emacs}" "${h_max_browser}" "${v_max_browser}" "${h_max_terminal}"  "${v_max_terminal}" 
+         
+    if [ "${h_max_emacs}" == "no" -a "${v_max_emacs}" == "yes" -a \
+         "${h_max_browser}" == "yes" -a "${v_max_browser}" == "yes" -a \
+         "${h_max_terminal}" == "no" -a "${v_max_terminal}" == "yes" ] ; then
+        if [ "${current_focused_window}" == "${emacs}" -o "${current_focused_window}" == "${terminal}" ] ; then
+            wmctrl -i -a "${browser}"
+        else
+            wmctrl -i -a "${terminal}"
+            wmctrl -i -a "${emacs}"
+        fi
+    elif [ "${h_max_emacs}" == "no" -a "${v_max_emacs}" == "yes" -a \
+           "${h_max_browser}" == "no" -a "${v_max_browser}" == "yes" -a \
+           "${h_max_terminal}" == "yes" -a "${v_max_terminal}" == "yes" ] ; then
+        if [ "${current_focused_window}" == "${emacs}" -o "${current_focused_window}" == "${browser}" ] ; then
+            wmctrl -i -a "${terminal}"
+        else
+            wmctrl -i -a "${browser}"
+            wmctrl -i -a "${emacs}"
+        fi
+    elif [ "${h_max_emacs}" == "yes" -a "${v_max_emacs}" == "yes" -a \
+           "${h_max_browser}" == "no" -a "${v_max_browser}" == "yes" -a \
+           "${h_max_terminal}" == "no" -a "${v_max_terminal}" == "yes" ] ; then
+        if [ "${current_focused_window}" == "${terminal}" -o "${current_focused_window}" == "${browser}" ] ; then
+            wmctrl -i -a "${emacs}"
+        else
+            wmctrl -i -a "${browser}"
+            wmctrl -i -a "${terminal}"
+        fi
+    else
         if [ "${current_focused_window}" == "${emacs}" ] ; then
             wmctrl -i -a "${terminal}"
         elif [ "${current_focused_window}" == "${terminal}" ] ; then
             wmctrl -i -a "${browser}"
         else
-            wmctrl -i -a "${emacs}"
-        fi
-    else
-        if [ "${current_focused_window}" == "${emacs}" -o "${current_focused_window}" == "${terminal}" ] ; then
-            wmctrl -i -a "${browser}"
-        else
-            wmctrl -i -a "${terminal}"
             wmctrl -i -a "${emacs}"
         fi
     fi
